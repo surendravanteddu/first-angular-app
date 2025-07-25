1565d3736337667ed8191197147d9b9c8b8a02f4
Surendra Vanteddu <surendra.vanteddu@moodys.com>
2025-07-24 23:08:54 -0500

    add todos route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,8 @@ export const routes: Routes = [{
   path: '',
   pathMatch: 'full',
   loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+}, {
+  path: 'todos',
+  pathMatch: 'full',
+  loadComponent: () => import('./todos/todos.component').then(m => m.TodosComponent)
 }];
